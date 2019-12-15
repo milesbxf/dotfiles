9160a0aa9d9951c1e998ebfc0c4b83a7b7bae99c
Miles Bryant <milesbryant@monzo.com>
2019-12-15 11:40:32 +0000

    Optimise autocompletion init

--- shell-startup/2-aliases.zsh
+++ shell-startup/2-aliases.zsh
@@ -1,9 +1,5 @@
 #!/usr/bin/env zsh
 
-# Load ZSH tab completion
-autoload -Uz compinit
-compinit
-
 
 # Add scripts to paths
 export PATH="${HOME}/.dotfiles/scripts:${PATH}:/usr/local/kubebuilder/bin"
