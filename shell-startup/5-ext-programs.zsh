c5875670f737d843474ac72052cc2292627ec821
Miles Bryant <milesbryant@monzo.com>
2019-12-15 11:24:22 +0000

    Lazy load completions

--- shell-startup/5-ext-programs.zsh
+++ shell-startup/5-ext-programs.zsh
@@ -3,7 +3,6 @@
 # Load FZF (https://github.com/junegunn/fzf)
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-
 load_virtualenvwrapper() {
     if [[ -z $VIRTUALENVWRAPPER_SCRIPT ]]; then
         source /usr/local/bin/virtualenvwrapper.sh
@@ -19,25 +18,48 @@ rmvirtualenv() {
     rmvirtualenv ${@}
 }
 
-# Kubectl completion
+# Check if 'kubectl' is a command in $PATH
 if [ $commands[kubectl] ]; then
-  source <(kubectl completion zsh)
+
+  # Placeholder 'kubectl' shell function:
+  # Will only be executed on the first call to 'kubectl'
+  kubectl() {
+
+    # Remove this function, subsequent calls will execute 'kubectl' directly
+    unfunction "$0"
+
+    # Load auto-completion
+    source <(kubectl completion zsh)
+
+    # Execute 'kubectl' binary
+    $0 "$@"
+  }
+fi
+
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/milesbryant/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/milesbryant/google-cloud-sdk/path.zsh.inc'; fi
+
+# Check if 'kubectl' is a command in $PATH
+if [ $commands[gcloud] ]; then
+
+  # Placeholder 'kubectl' shell function:
+  # Will only be executed on the first call to 'kubectl'
+  gcloud() {
+
+    # Remove this function, subsequent calls will execute 'kubectl' directly
+    unfunction "$0"
+
+    # Load auto-completion
+    if [ -f '/Users/milesbryant/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/milesbryant/google-cloud-sdk/completion.zsh.inc'; fi
+
+    # Execute 'gcloud' binary
+    $0 "$@"
+  }
 fi
 
 #GOPATH should be home dir
 export GOPATH=~
 # export GOFLAGS="-mod=vendor" 
 # export GO111MODULE=on
-
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/milesbryant/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/milesbryant/google-cloud-sdk/path.zsh.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '/Users/milesbryant/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/milesbryant/google-cloud-sdk/completion.zsh.inc'; fi
-
-
-# added by travis gem
-[ -f /Users/milesbryant/.travis/travis.sh ] && source /Users/milesbryant/.travis/travis.sh
-
 
